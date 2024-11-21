5d390b4a59f67e853c4ff704d092054a87eefe0d
Dhruv Pal <dhruvpal.4818@gmail.com>
2024-11-21 21:56:09 +0530

    add getting data from gemini functionality

--- src/dashboard/resume/component/forms/Summary.jsx
+++ src/dashboard/resume/component/forms/Summary.jsx
@@ -5,12 +5,17 @@ import { useContext, useEffect, useState } from "react";
 import GlobalApi from "../../../../../service/GlobalApi";
 import { useParams } from "react-router-dom";
 import { Bot, Brain, LoaderCircle } from "lucide-react";
+import { AIChatSession } from "./../../../../../service/AIModal";
+
+const prompt =
+  "Job Title: {jobTitle} , Depends on job title give me list of  summery for 3 experience level, Mid Level and Freasher level in 3 -4 lines in array format, With summery and experience_level Field in JSON Format";
 
 export default function Summary({ enableNext }) {
   const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
   const [summary, setSummary] = useState();
   const [loading, setLoading] = useState(false);
   const params = useParams();
+  const [aiGeneratedSummaryList, setAiGeneratedSummaryList] = useState([]);
 
   useEffect(() => {
     summary &&
@@ -19,6 +24,16 @@ export default function Summary({ enableNext }) {
         summery: summary,
       });
   }, [summary]);
+
+  const GenerateSummaryFromAI = async () => {
+    setLoading(true);
+    const PROMPT = prompt.replace("jobTitle", resumeInfo?.jobTitle);
+    console.log(PROMPT);
+    const result = await AIChatSession.sendMessage(PROMPT);
+    const parsedResponse = JSON.parse(result.response.text());
+    setAiGeneratedSummaryList(parsedResponse.summaries);
+    setLoading(false);
+  };
 
   const onSave = (e) => {
     e.preventDefault();
@@ -49,7 +64,12 @@ export default function Summary({ enableNext }) {
         <form onSubmit={onSave}>
           <div className="flex justify-between items-end mt-7">
             <label className="font-semibold">Add Summary</label>
-            <Button size="sm" className="bg-purple-500 text-bold" type="button">
+            <Button
+              size="sm"
+              className="bg-purple-500 text-bold"
+              type="button"
+              onClick={() => GenerateSummaryFromAI()}
+            >
               <span className="flex justify-between items-center gap-2 text-white">
                 Generate from AI <Bot />
               </span>
@@ -70,6 +90,19 @@ export default function Summary({ enableNext }) {
             </Button>
           </div>
         </form>
+        {aiGeneratedSummaryList && (
+          <div>
+            <h2 className="font-bold text-lg">Suggestions</h2>
+            {aiGeneratedSummaryList.map((item, index) => (
+              <div>
+                <h2 className="font-bold my-1">
+                  Level: {item?.experience_level}
+                </h2>
+                <p>{item?.summary}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
